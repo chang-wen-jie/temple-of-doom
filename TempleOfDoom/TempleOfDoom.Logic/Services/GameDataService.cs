7fadd0564fe109c98ce898ee0e27bef205159c39
chang-wen-jie <changwenjie1999@gmail.com>
2024-12-14 14:15:46 +0100

    add grid layout for rooms and items and doors

--- TempleOfDoom/TempleOfDoom.Logic/Services/GameDataService.cs
+++ TempleOfDoom/TempleOfDoom.Logic/Services/GameDataService.cs
@@ -8,29 +8,24 @@ namespace TempleOfDoom.Logic.Services
         public static GameDataDTO LoadGameData(string filePath)
         {
             string json = File.ReadAllText(filePath);
-            GameDataDTO? gameData = JsonSerializer.Deserialize<GameDataDTO>(json) ?? throw new Exception("Kan gameData.json niet inlezen");
-
+            GameDataDTO gameData = JsonSerializer.Deserialize<GameDataDTO>(json) ?? throw new Exception("Kan gameData.json niet inlezen");
             return gameData;
         }
 
-        public static Player CreatePlayer(GameDataDTO gameData)
+        public static Player CreatePlayer(Player player)
         {
-            Player newPlayer = new()
+            return new Player
             {
-                startRoomId = gameData.player.startRoomId,
-                startX = gameData.player.startX,
-                startY = gameData.player.startY,
-                lives = gameData.player.lives,
+                startRoomId = player.startRoomId,
+                startX = player.startX,
+                startY = player.startY,
+                lives = player.lives,
             };
-
-            return newPlayer;
         }
 
-        public static Room CreateRoom(GameDataDTO gameData, int roomId)
+        public static Room CreateRoom(Room room)
         {
-            Room room = gameData.rooms.FirstOrDefault(r => r.id == roomId) ?? throw new Exception($"Room ID {roomId} bestaat niet");
-
-            Room newRoom = new()
+            return new Room
             {
                 id = room.id,
                 type = room.type,
@@ -43,36 +38,30 @@ namespace TempleOfDoom.Logic.Services
                     x = i.x,
                     y = i.y,
                     color = i.color,
-                }).ToArray()
-            };
-
-            return newRoom;
-        }
-
-        public static Connection CreateConnection(GameDataDTO gameData, int northRoomId, int southRoomId)
-        {
-            var connection = gameData.connections.FirstOrDefault(c =>
-                (c.NORTH == northRoomId && c.SOUTH == southRoomId) ||
-                (c.NORTH == southRoomId && c.SOUTH == northRoomId) ||
-                (c.WEST == northRoomId && c.EAST == southRoomId) ||
-                (c.WEST == southRoomId && c.EAST == northRoomId)) ??
-                throw new Exception($"Geen connectie tussen Rooms IDS {northRoomId} en {southRoomId}");
-
-            Connection newConnection = new()
-            {
-                NORTH = connection.NORTH,
-                SOUTH = connection.SOUTH,
-                WEST = connection.WEST,
-                EAST = connection.EAST,
-                doors = connection.doors.Select(d => new Door
-                {
-                    type = d.type,
-                    color = d.color,
-                    no_of_stones = d.no_of_stones,
                 }).ToArray(),
             };
+        }
 
-            return newConnection;
+        public static Connection CreateConnection(Connection connectionDTO)
+        {
+            return new Connection
+            {
+                NORTH = connectionDTO.NORTH,
+                SOUTH = connectionDTO.SOUTH,
+                EAST = connectionDTO.EAST,
+                WEST = connectionDTO.WEST,
+                doors = connectionDTO.doors.Select(d => CreateDoor(d)).ToArray(),
+            };
+        }
+
+        public static Door CreateDoor(Door doorDTO)
+        {
+            return new Door
+            {
+                type = doorDTO.type,
+                color = doorDTO.color,
+                no_of_stones = doorDTO.no_of_stones,
+            };
         }
     }
 }
