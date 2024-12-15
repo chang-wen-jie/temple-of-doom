732614370dc292955c1a925e435230c40a530233
N1TROGUE <n1trogue@protonmail.com>
2024-12-15 16:04:39 +0100

    Removed BaseDecorator
    Removed BaseDecorator as it just complicates things imo

--- TempleOfDoom/TempleOfDoom.Logic/Decorators/ClosingDecorator.cs
+++ TempleOfDoom/TempleOfDoom.Logic/Decorators/ClosingDecorator.cs
@@ -1,29 +1,41 @@
 ﻿namespace TempleOfDoom.Logic.Decorators;
 
-public class ClosingDecorator : DoorDecorator
+public class ClosingDecorator : IDoor
 {
+    private IDoor _wrappee;
+    public int Id => _wrappee.Id;
+    public bool IsOpen => _wrappee.IsOpen;
+    public Direction Direction => _wrappee.Direction;
+    public Room firstRoom => _wrappee.firstRoom;
+    public Room secondRoom => _wrappee.secondRoom;
     private bool _hasBeenOpened;
     
-    public ClosingDecorator(IDoor wrappee) : base(wrappee)
+    public ClosingDecorator(IDoor wrappee)
     {
+        this._wrappee = wrappee;
         this._hasBeenOpened = false;
     }
-
-    public override void Update()
-    {
-        throw new NotImplementedException();
-    }
     
-    public new void Open()
+    public void Open()
     {
         if (!_hasBeenOpened)
         {
-            base.Open();
+            _wrappee.Open();
             _hasBeenOpened = true;
         }
         else
         {
-            base.Close();
+            _wrappee.Close();
         }
+    }
+
+    public void Close()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void Update()
+    {
+        throw new NotImplementedException();
     }
 }
--- TempleOfDoom/TempleOfDoom.Logic/IDoor.cs
+++ TempleOfDoom/TempleOfDoom.Logic/IDoor.cs
@@ -4,6 +4,9 @@ public interface IDoor : IObserver
 {
     int Id { get; }
     bool IsOpen { get; }
+    Direction Direction { get; }
+    Room firstRoom { get; }
+    Room secondRoom { get; }
     
     void Open();
     void Close();
