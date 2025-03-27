725c53527635c5b892b8a2fee0d474d0ff27e4b1
N1TROGUE <n1trogue@protonmail.com>
2025-03-27 12:52:43 +0100

    Added functionality to add and remove Items from player

--- TempleOfDoom/TempleOfDoom.Logic/Models/Player.cs
+++ TempleOfDoom/TempleOfDoom.Logic/Models/Player.cs
@@ -1,22 +1,41 @@
-﻿namespace TempleOfDoom.Logic.Models;
+﻿using TempleOfDoom.Logic.Models.Items;
+
+namespace TempleOfDoom.Logic.Models;
 
 public class Player
 {
-    public int startRoomId { get; private set; }
-    public int startXPos { get; private set; }
-    public int startYPos { get; private set; }
-    public int lives { get; private set; }
+    public int StartRoomId { get; private set; }
+    public int StartXPos { get; private set; }
+    public int StartYPos { get; private set; }
+    private int Lives { get; set; }
+    private List<IItem> Items { get; set; }
     
     public Player(int startRoomId, int startXPos, int startYPos, int lives)
     {
-        this.startRoomId = startRoomId;
-        this.startXPos = startXPos;
-        this.startYPos = startYPos;
-        this.lives = lives;
+        this.StartRoomId = startRoomId;
+        this.StartXPos = startXPos;
+        this.StartYPos = startYPos;
+        this.Lives = lives;
+        Items = new();
     }
 
     public void decreaseLives(int amount)
     {
-        if (amount <= lives) lives -= amount;
+        if (amount <= Lives) Lives -= amount;
+    }
+
+    public void addItem(IItem item)
+    {
+        Items.Add(item);
+    }
+    
+    public IItem removeItem(IItem item)
+    {
+        return Items.Remove(item) ? item : null;
+    }
+    
+    public bool hasItem(IItem item)
+    {
+        return Items.Contains(item);
     }
 }
